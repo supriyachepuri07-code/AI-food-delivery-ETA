3901d011af06904cfe1e8e7c2e0d7887371e5b36
Chepuri Supriya <chepuri.supriya@brillio.com>
2026-07-28 18:10:26 +0530

    add tes file

---
+++ config/test_scripts/test.paths.py
@@ -0,0 +1,33 @@
+from config.paths import *
+
+print("=" * 50)
+print("PROJECT ROOT")
+print(PROJECT_ROOT)
+
+print("=" * 50)
+print("DATA DIRECTORY")
+print(DATA_DIR)
+
+print("=" * 50)
+print("RAW DATA")
+print(RAW_DATA_DIR)
+
+print("=" * 50)
+print("PROCESSED DATA")
+print(PROCESSED_DATA_DIR)
+
+print("=" * 50)
+print("MODELS")
+print(MODELS_DIR)
+
+print("=" * 50)
+print("LOGS")
+print(LOGS_DIR)
+
+print("=" * 50)
+print("SOURCE")
+print(SRC_DIR)
+
+print("=" * 50)
+print("CONFIG")
+print(CONFIG_DIR)
