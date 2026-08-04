3f703eff1d17bbd01949a9311156441fe19bc4ec
Chepuri Supriya <chepuri.supriya@brillio.com>
2026-08-04 18:11:00 +0530

    add datavalidationconfig

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,5 +1,8 @@
 from config.paths import RAW_DATA_DIR, PROCESSED_DATA_DIR
-from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+)
 
 
 class ConfigurationManager:
@@ -17,3 +20,12 @@ class ConfigurationManager:
         )
 
         return ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+
+        validation_config = DataValidationConfig(
+            train_data_path=PROCESSED_DATA_DIR / "train.csv",
+            test_data_path=PROCESSED_DATA_DIR / "test.csv"
+        )
+
+        return validation_config
