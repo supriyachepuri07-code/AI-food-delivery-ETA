9224670e29770a6b2d5304ceada503f4f529adf9
Chepuri Supriya <chepuri.supriya@brillio.com>
2026-08-05 11:09:34 +0530

    add datatransformationconfig

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -19,3 +19,12 @@ class DataValidationConfig:
 
     train_data_path: Path
     test_data_path: Path
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    """
+    Configuration for the Data Transformation component.
+    """
+
+    train_data_path: Path
+    test_data_path: Path
+    preprocessor_pbject_file_path: Path
